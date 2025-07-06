ba547f7805371c72e686d0be510a9d0439fc0eb4
samarth sharma <samarths716@gmail.com>
2025-07-06 20:17:22 +0530

    style the active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,7 @@
+"use client"
+import {clsx as cn} from 'clsx'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { IoBugOutline } from "react-icons/io5";
 const links = [
     {
@@ -11,6 +14,7 @@ const links = [
     }
 ]
 function NavBar() {
+    const pathname  = usePathname();
   return (
     <nav className="flex space-x-4 h-12 border-b border-zinc-200 items-center px-4 mb-2">
         <Link href="/" className="text-2xl">
@@ -19,7 +23,7 @@ function NavBar() {
         <ul className="flex space-x-4 text-lg font-medium">
             {
                 links.map((link)=>(
-                    <Link className="text-zinc-500 hover:text-zinc-800 transition-colors" key={link.name} href={link.href}>{link.name}</Link>
+                    <Link className={cn("text-zinc-500 hover:text-zinc-800 transition-colors",pathname === link.href && "text-zinc-800")} key={link.name} href={link.href}>{link.name}</Link>
                 ))
             }
         </ul>
